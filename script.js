d2c688ee658e08e059284533e5f8d1fc04f0fd6d
trivedijeee <trivedijeee@gmail.com>
2024-05-23 18:46:44 +0530

    toogle bar update and contract page footer update

--- script.js
+++ script.js
@@ -64,4 +64,39 @@ document.addEventListener('DOMContentLoaded', function () {
       // Scroll to the "Skills" section
       document.getElementById('skillsSection').scrollIntoView({ behavior: 'smooth' });
     });
+// when scroll removing the oepn item 
+    window.addEventListener('scroll', function() {
+      var menuToggle = document.getElementById('menu-toggle');
+      if (window.scrollY > 50) {
+          menuToggle.checked = false; // Hide the toggle if scrolled more than 50px
+      }
+  }); // scroll option ended
+
+
+  // hide toogle when someone click out the menu item not inside the menu item
+  var menuToggle = document.getElementById('menu-toggle');
+  var menuIcon = document.querySelector('.menu-icon');
+
+  // Function to check if the click occurred outside the toggle option
+  function isClickOutsideToggle(event) {
+      if (!menuIcon.contains(event.target) && event.target !== menuToggle) {
+          menuToggle.checked = false; // Hide the toggle
+          document.removeEventListener('click', isClickOutsideToggle); // Remove the click event listener
+      }
+  }
+
+  // Function to handle menu toggle click event
+  function handleMenuToggleClick() {
+      if (menuToggle.checked) {
+          document.addEventListener('click', isClickOutsideToggle); // Add click event listener to check for clicks outside the toggle
+      }
+  }
+
+  // Attach event listener to the menu toggle
+  menuToggle.addEventListener('click', handleMenuToggleClick);
+
+  // Prevent hiding the toggle when clicking inside the menu area
+  document.querySelector('nav').addEventListener('click', function(event) {
+      event.stopPropagation(); // Prevent the click event from bubbling up to the document
+  }); // click event enable done
 });
